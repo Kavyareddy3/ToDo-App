b762dc68df1cb684ecc04906127a38ebf03451fa
Budidha Kavya <kavyareddybudidha@gmail.com>
2025-11-23 11:08:53 +0530

    ToDoApp.java

--- ToDoApp.java
+++ ToDoApp.java
@@ -1,44 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
 public class ToDoApp extends JFrame {
-
     private DefaultListModel<String> listModel;
     private JList<String> taskList;
     private JTextField taskField;
     private JButton addButton, deleteButton;
-
     public ToDoApp() {
         setTitle("To-Do List App");
         setSize(400, 450);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-
         // Top Panel (Input area)
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new FlowLayout());
-
         taskField = new JTextField(20);
         addButton = new JButton("Add Task");
-
         topPanel.add(taskField);
         topPanel.add(addButton);
-
         // Center Panel (Task List)
         listModel = new DefaultListModel<>();
         taskList = new JList<>(listModel);
         JScrollPane scrollPane = new JScrollPane(taskList);
-
         // Bottom Panel (Delete button)
         JPanel bottomPanel = new JPanel();
         deleteButton = new JButton("Delete Task");
         bottomPanel.add(deleteButton);
-
         add(topPanel, BorderLayout.NORTH);
         add(scrollPane, BorderLayout.CENTER);
         add(bottomPanel, BorderLayout.SOUTH);
-
         // Action Listeners
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -51,7 +41,6 @@ public class ToDoApp extends JFrame {
                 }
             }
         });
-
         deleteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 int selectedIndex = taskList.getSelectedIndex();
@@ -62,10 +51,8 @@ public class ToDoApp extends JFrame {
                 }
             }
         });
-
         setVisible(true);
     }
-
     public static void main(String[] args) {
         new ToDoApp();
     }
